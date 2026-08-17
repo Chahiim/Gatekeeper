8c6d1eecfcb22e673bba58c95f797f9c201c8f68
Chahiim Pop <2020152199@ub.edu.bz>
2026-08-17 07:59:00 -0600

    Docs: Implemented helper functions

---
+++ cmd/api/helpers.go
@@ -0,0 +1,21 @@
+// Filename: cmd/api/helpers.go
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	// Use the "ParmsFromContext" function to get the parameters from the request context as a slice
+	params := httprouter.ParamsFromContext(r.Context())
+	// GET the value of the "id" parameter
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid ID parameter")
+	}
+	return id, nil
+}
